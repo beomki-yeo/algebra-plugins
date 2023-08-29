1ff6e0c64bb1c8ebd9ca82520e0ac2853d241418
beomki-yeo <beomki.yeo@gmail.com>
2023-08-29 22:33:26 +0200

    Backup

--- tests/accelerator/sycl_fpga/array_cmath.cpp
+++ tests/accelerator/sycl_fpga/array_cmath.cpp
@@ -9,8 +9,8 @@
 #include "algebra/array_cmath.hpp"
 
 // SYCL include(s).
-#include <sycl/sycl.hpp>
 #include <sycl/ext/intel/fpga_extensions.hpp>
+#include <sycl/sycl.hpp>
 
 // System include(s).
 #include <iostream>
@@ -19,14 +19,14 @@ using namespace sycl;
 
 // Simple asynchronous handler function
 auto handle_async_error = [](::sycl::exception_list elist) {
-    for (auto& e : elist) {
-        try {
-            std::rethrow_exception(e);
-        } catch (::sycl::exception& e) {
-            std::cout << "ASYNC EXCEPTION!!\n";
-            std::cout << e.what() << "\n";
-        }
+  for (auto& e : elist) {
+    try {
+      std::rethrow_exception(e);
+    } catch (::sycl::exception& e) {
+      std::cout << "ASYNC EXCEPTION!!\n";
+      std::cout << e.what() << "\n";
     }
+  }
 };
 
 int main() {
@@ -47,12 +47,30 @@ int main() {
   std::vector<float> vec_b(kArraySize);
   std::vector<float> vec_r(kArraySize);
 
-  // Create a queue bound to the chosen device.
-  // If the device is unavailable, a SYCL runtime exception is thrown.
-  queue q(selector, handle_async_error);
+  try {
 
-  auto device = q.get_device();
+    // Create a queue bound to the chosen device.
+    // If the device is unavailable, a SYCL runtime exception is thrown.
+    queue q(selector, handle_async_error);
 
-  std::cout << "Running on device: "
-            << device.get_info<sycl::info::device::name>().c_str() << std::endl;
+    auto device = q.get_device();
+
+    std::cout << "Running on device: "
+              << device.get_info<sycl::info::device::name>().c_str()
+              << std::endl;
+
+  } catch (exception const& e) {
+    // Catches exceptions in the host code
+    std::cerr << "Caught a SYCL host exception:\n" << e.what() << "\n";
+
+    // Most likely the runtime couldn't find FPGA hardware!
+    if (e.code().value() == CL_DEVICE_NOT_FOUND) {
+      std::cerr << "If you are targeting an FPGA, please ensure that your "
+                   "system has a correctly configured FPGA board.\n";
+      std::cerr << "Run sys_check in the oneAPI root directory to verify.\n";
+      std::cerr << "If you are targeting the FPGA emulator, compile with "
+                   "-DFPGA_EMULATOR.\n";
+    }
+    std::terminate();
+  }
 };
